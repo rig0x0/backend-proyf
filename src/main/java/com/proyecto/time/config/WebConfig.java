18293842bc8748e0f252a8337fe86f4420709f32
rig0x0 <128100551+rig0x0@users.noreply.github.com>
2025-01-14 09:53:52 -0600

    Update2 WebConfig.java

--- src/main/java/com/proyecto/time/config/WebConfig.java
+++ src/main/java/com/proyecto/time/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Permitimos todas las rutas
-                .allowedOrigins("http://localhost:4200","https://frontend-proyf.vercel.app")  // Permitimos el origen de tu frontend Angular
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
-                .allowedHeaders("*");  // Permitimos cualquier cabecera
+        registry.addMapping("/**")  // Aplica CORS a todas las rutas
+                .allowedOrigins("http://localhost:4200", "https://frontend-proyf.vercel.app") // Dominios permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permitir todas las cabeceras
+                .allowCredentials(true); // Habilitar cookies y credenciales
     }
 }
